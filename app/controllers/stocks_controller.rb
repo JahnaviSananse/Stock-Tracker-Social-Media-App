cb236106e7ffff6ea00511626a4fce37cd83df19
JahnaviSananse <jahnavisananse@gmail.com>
2021-08-23 12:04:10 +0530

    Dealing with Invalid search result

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,18 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    # Stock.new_look -> Stock is class and new_look is method of that class from stock.rb
-    # render json: @stock
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      # Stock.new_look -> Stock is class and new_look is method of that class from stock.rb
+      # render json: @stock
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = 'Please enter a valid symbol to search'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = 'Please enter a symbol to search'
+      redirect_to my_portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,11 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
     # client.price(ticker_symbol)
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,
-        last_price: client.price(ticker_symbol))
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,
+          last_price: client.price(ticker_symbol))
+    rescue StandardError => e
+      nil
+    end
   end
 end
