4c3276979ed64939d66a7baef8596d392ecc5999
JahnaviSananse <jahnavisananse@gmail.com>
2021-08-23 11:51:21 +0530

    Create and display stock objects in the browser

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,8 @@
 class StocksController < ApplicationController
   def search
-    stock = Stock.new_lookup(params[:stock])
+    @stock = Stock.new_lookup(params[:stock])
     # Stock.new_look -> Stock is class and new_look is method of that class from stock.rb
-    render json: stock
+    # render json: @stock
+    render 'users/my_portfolio'
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,8 @@ class Stock < ApplicationRecord
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.price(ticker_symbol)
+    # client.price(ticker_symbol)
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,
+        last_price: client.price(ticker_symbol))
   end
 end
